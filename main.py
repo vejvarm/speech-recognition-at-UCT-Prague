5fd2246400d3e44647ac046e5560a9642b10a54e
Unknown <vejvarm@gmail.com>
2018-10-11 21:08:30 +0200

    rewriting make_frames function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from scipy.io import wavfile
 from scipy.signal import stft
 from matplotlib import pyplot as plt
 
+
 def plot_signal(time, signal, title=''):
     plt.plot(time, signal)
     plt.title(title)
@@ -11,15 +12,46 @@ def plot_signal(time, signal, title=''):
     plt.ylabel('Amplitude')
     plt.grid(True, 'major')
 
+
 def pre_emphasis(signal, alpha=0.95):
     """Applies preemphasis filter on the signal which boosts the high frequencies"""
-    return np.append(signal[0], signal[1:] - alpha*signal[:-1])
+    return np.append(signal[0], signal[1:] - alpha * signal[:-1])
+
 
 def extract_mono(signal):
     """Extract single channel from a stereo signal waveform"""
     return signal[:, 0]
 
+
 def make_frames(signal, frequency, width=0.025, stride=0.01):
+    """divide the time-based signal into frames with specific width and stride
+    :param signal: time-domain signal to be divided into frames
+    :param width: the width of one frame in seconds
+    :param stride: the stride at which the frames are made in seconds
+    :return: array (frame_length,n_frames) of individual frames
+    """
+
+    sqn_len = len(signal)
+    frame_len = int(width * frequency)
+    stride_len = int(stride * frequency)
+    n_frames = np.array(np.ceil((sqn_len - frame_len) / stride_len) + 1, dtype=np.int32)
+
+    frames = [signal[i * stride_len:i * stride_len + frame_len].T for i in range(n_frames)]
+
+    print(np.shape(frames))
+    print(len(frames[-1]))
+
+    # last frame should be padded to same length as the other frames
+    if len(frames[-1]) < frame_len:
+        frames[-1] = np.pad(frames[-1], (0, frame_len - len(frames[-1])), 'constant', constant_values=0)
+        print(np.shape(frames[-1]))
+    else:
+        last_frame = None
+
+    return np.vstack(frames)
+
+
+def make_frames_stolen(signal, frequency, width=0.025, stride=0.01):
     """divide the signal into frames with specific width and stride
 
     :param signal: time-domain signal to be divided into frames
@@ -28,7 +60,7 @@ def make_frames(signal, frequency, width=0.025, stride=0.01):
     :return: array of individual frames
     """
 
-    frame_length, frame_step = frequency*width, int(frequency*stride)
+    frame_length, frame_step = frequency * width, int(frequency * stride)
     signal_length = len(signal)
     frame_length = int(round(frame_length))
     num_frames = int(np.ceil(
@@ -44,22 +76,26 @@ def make_frames(signal, frequency, width=0.025, stride=0.01):
 
     return pad_signal[indices.astype(np.int32, copy=False)]
 
+
 def hamming(frame):
     """Apply Hamming window on input frame"""
-    return 0.54 - 0.46*np.cos(2*np.pi*frame/(len(frame)-1))
+    return 0.54 - 0.46 * np.cos(2 * np.pi * frame / (len(frame) - 1))
+
 
 def make_stereo(signal):
     """Makes a stereo signal (2 channels) from mono signal"""
     return np.vstack((signal, signal)).T
 
+
 def short_time_ft(signal, frequency):
     """Apply short time fourier transform directly on the input signal"""
     return stft(signal, fs=frequency, window='hamming')
 
+
 def fourier_transform(frames):
     """Apply fast fourier transform to frames of input signal"""
-    NFFT = np.shape(frames)[1]
-    return np.abs(np.fft.fft(frames,NFFT))  # TODO: not right - do it over
+    n_fft = np.shape(frames)[1]
+    return np.abs(np.fft.fft(frames, n_fft))  # TODO: not right - do it over
 
 
 if __name__ == '__main__':
@@ -69,7 +105,7 @@ if __name__ == '__main__':
 
     sig_len = np.shape(signal)[0]
 
-    time = np.linspace(0, sig_len/frequency, sig_len)
+    time = np.linspace(0, sig_len / frequency, sig_len)
 
     # applying pre-emphasis filter on signal
     pre_emphasised_signal = pre_emphasis(signal)
@@ -103,4 +139,4 @@ if __name__ == '__main__':
     stereo_pre_emph = make_stereo(pre_emphasised_signal)
     print(np.shape(stereo_pre_emph))  # TODO: remove print
 
- #   wavfile.write('data/preempth_ucisedobre.wav', frequency, stereo_pre_emph)
+#   wavfile.write('data/preempth_ucisedobre.wav', frequency, stereo_pre_emph)
