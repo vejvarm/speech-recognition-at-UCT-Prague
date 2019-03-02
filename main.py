1ec8889d47a077af76d303614bf97e495430139b
Vejvar Martin <vejvarm@vscht.cz>
2019-03-02 23:32:24 +0100

    refactor: update config for new computing system

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from models.AcousticModel import AcousticModel
 
-# TODO: fix exploding loss values at second epoch --> gradient_clipping
+# TODO: placeholder for grad_clip_val (should increase with time length?)
+# TODO: suffle the order of batches (keeps similar lengths in batch but adds variability through epochs)
+# TODO: Gradient masking on feed forward layers https://stackoverflow.com/questions/43364985/how-to-stop-gradient-for-some-entry-of-a-tensor-in-tensorflow
+# TODO: https://www.tensorflow.org/api_docs/python/tf/custom_gradient
 if __name__ == '__main__':
     config_path = "./config"
     ac_model = AcousticModel(config_path)
